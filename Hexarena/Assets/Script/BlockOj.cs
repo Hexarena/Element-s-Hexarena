a15215fc3a79c9d3baa1a5e4f237c0945f209f33
votruongan <32063562+votruongan@users.noreply.github.com>
2017-10-23 20:50:25 +0700

    [VTAn] - Summon Figure if Invalid value == 2
    BlockManage.cs:
        - Thêm hàm getBlockValid(x,y) để lấy giá trị Invalid của
        ô x,y
    BlockOj.cs:
        -  Thêm 2 biến This_x, This_y lưu tọa độ của ô.
        -  Thêm biến Manager để liên lạc với GameManage.
        -  Trong Start: tính toán tọa độ của ô và lưu vào This_x,
        This_y để tiện sử dụng sau này.
        - Start sẽ tìm GameManage cho Manager.
        - Trong OnMouseOver: thêm điều kiện nếu giá trị Invalid
       của ô == 2 thì Instantiate Figure. (Sử dụng switch để lỡ
       sau này còn làm gì khác.)
       -  Trong getPosition: đã sửa lại theo kiểu tận dụng các
       biến This_x và This_y.

--- Hexarena/Assets/Script/BlockOj.cs
+++ Hexarena/Assets/Script/BlockOj.cs
@@ -5,55 +5,70 @@ using UnityEngine;
 public class BlockOj : MonoBehaviour {
 	Animator anim;
 	GameObject obj;
+	public byte This_x, This_y; //Position of this block
+	public BlockManage Manager; //The GameManage
     public GameObject goFigure;
-    bool isHasFigure = false;
-	// Use this for initialization
+	//bool isHasFigure = false;
+
+	//Run once the object is created
 	void Start () {
 		obj = gameObject;
+		//Find the GameManage, the controller of the game
+		Manager = GameObject.Find ("GameManage").GetComponent<BlockManage>();
 		anim = GetComponent<Animator>();
 		anim = obj.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+		//Find the position of this block for further usage
+		var FigureName = obj.name.Split('_');
+		This_x = (byte)(int)System.Double.Parse(FigureName[1]);
+		This_y = (byte)(int)System.Double.Parse(FigureName[2]);
 	}
 
+	//Mouse Stay in the block	
     private void OnMouseOver()
     {
-        if (Input.GetMouseButtonUp(0))
-        if (!isHasFigure)
-        {
-            isHasFigure = true;
-            Instantiate(goFigure,
-                getPosition(obj.name),
-                Quaternion.identity)
-                .transform.SetParent(this.transform);
-        }
+		//Receive left-click
+		if (Input.GetMouseButtonUp (0)) {
+			//Find this block's Invalid value from GameManage
+			byte Invalid = Manager.getBlockValid(This_x,This_y);
+			Debug.Log ("Invalid VALUE = " + Invalid);
+			//Perform action according to Invalid value
+			switch(Invalid){
+			case 2: 
+				//Summon Figure when value is 2
+					Instantiate (goFigure,
+						getPosition (),
+						Quaternion.identity)
+						.transform.SetParent (this.transform);
+					break;
+				//Otherwise
+				default: //Do something
+					break;
+			}
+		}
     }
 
+	//Mouse Enter the block
     void OnMouseEnter()
 	{
-		anim.SetInteger("Status", 1);
+		anim.SetInteger("Status", 1);//Change this block status
 		Debug.Log("Vo "+obj.name);
 	}
-
+	//Mouse Exit the block
 	void OnMouseExit()
 	{
-		anim.SetInteger("Status", 0);
+		anim.SetInteger("Status", 0);//Change this block status
 		Debug.Log("Raa");
 	}
 
-    Vector2 getPosition(string name)
-    {
-        var FigureName = name.Split('_');
-        float x = (float)System.Double.Parse(FigureName[1]);
-        float y = (float)System.Double.Parse(FigureName[2]);
-        x = x * BlockManage.xOffset - 10.5f +
-            BlockManage.xOffset * (y / 2f) +
-            BlockManage.xOffset;
-        y = y * BlockManage.yOffset -
-            7 * BlockManage.yOffset / 2f;
-        return new Vector2(x, y);
-    }
+	Vector2 getPosition()
+	{
+		float x = This_x * BlockManage.xOffset - 10.5f 
+					+ BlockManage.xOffset * (This_y / 2f) 
+					+ BlockManage.xOffset;
+		
+		float y = This_x * BlockManage.yOffset
+					- 7 * BlockManage.yOffset / 2f;
+		return new Vector2(x, y);
+	}
 }
