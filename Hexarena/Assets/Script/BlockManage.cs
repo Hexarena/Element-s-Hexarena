3af179a9852f6c9e9e9b3c608a9705d94ca811fe
nnvuf <nnvu.fit@hotmail.com>
2017-10-26 00:29:54 +0700

    Update Fix Error when Add figure, Add status for create Figure.

--- Hexarena/Assets/Script/BlockManage.cs
+++ Hexarena/Assets/Script/BlockManage.cs
@@ -4,36 +4,63 @@ using UnityEngine;
 
 public class BlockManage : MonoBehaviour {
 	public GameObject block;
-    private byte[,] Invalid;
-
-    private static int width = 11;
-    private static int height = 9;
-
-    public static float yOffset = -0.735f;
-    public static float xOffset = 1f;
-    public static float col = -xOffset - xOffset / 2;
-
-    void SetValid()
+    // mod start nnvu Change Invalid to static const
+    //private byte[,] Invalid
+    private static readonly byte[,] Invalid =
     {
-        Invalid = new byte[9, 11]
-        {
-            { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
-            { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
-            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 },
-        };
-    }
+        { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+        { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 },
+    };
+    // mod end nnvu
 
-	//Return the Invalid value of a block [x,y]
-	public byte getBlockValid(byte Pos_x, byte Pos_y){
+    // mod start nnvu Change type of size
+    private static readonly int width = 11;
+    private static readonly int height = 9;
+    //private static int width = 11;
+    //private static int height = 9;
+    // mod end nnvu
+
+    // mod start nnvu change yOffset, col, xOffset to static const
+    //public static float yOffset = -0.735f;
+    //public static float xOffset = 1f;
+    //public static float col = -xOffset - xOffset / 2;
+    private static readonly float yOffset = -0.735f;
+    private readonly static float xOffset = 1f;
+    private static float col = -xOffset - xOffset / 2;
+    // mod end nnvu
+
+    // del start nnvu remove SetInvalid
+    //void SetValid()
+    //{
+    //    Invalid = new byte[9, 11]
+    //    {
+    //        { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+    //        { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //        { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    //        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+    //        { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+    //        { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 },
+    //    };
+    //}
+    // del end nnvu remove SetInvalid
+
+    //Return the Invalid value of a block [x,y]
+    public byte getBlockValid(byte Pos_x, byte Pos_y){
 		return Invalid[Pos_x,Pos_y];
 	}
-    Animator anim;
+    // del start nnvu Delete amin
+    //Animator anim;
+    // del end nnvu
     void Start()
     {
         //hand = GameObject.Find("1:1");
@@ -44,7 +71,9 @@ public class BlockManage : MonoBehaviour {
         //}
         if (block != null)
         {
-            SetValid();
+            // del start nnvu remove SetInvalid
+            //SetValid();
+            // del end nnvu remve SetInvalid
             for (int jj = 0; jj < height; jj++)
             {
                 if ((jj % 2 == 0) && (jj != 0))
@@ -54,21 +83,28 @@ public class BlockManage : MonoBehaviour {
                     if (Invalid[jj, ii] == 0)
                     {
                         GameObject hex_go;
-                        if (jj % 2 == 0)
-                        {
-                            hex_go = (GameObject)Instantiate(
-                                block,
-                                new Vector2(ii * xOffset - 5.5f + col, jj * yOffset - 7*yOffset / 2f),
-                                Quaternion.identity);
-                        }
-                        else
-                        {
-                            hex_go = (GameObject)Instantiate(
-                                block,
-                                new Vector2(ii * xOffset + xOffset / 2 - 5.5f + col, jj * yOffset - 7*yOffset / 2f),
-                                Quaternion.identity);
-                        }
-
+                        // mod start nnvu Change How to create Base
+                        //if (jj % 2 == 0)
+                        //{
+                        //    hex_go = (GameObject)Instantiate(
+                        //        block,
+                        //        new Vector2(ii * xOffset - 5.5f + col, jj * yOffset - 7*yOffset / 2f),
+                        //        Quaternion.identity);
+                        //}
+                        //else
+                        //{
+                        //    hex_go = (GameObject)Instantiate(
+                        //        block,
+                        //        new Vector2(ii * xOffset + xOffset / 2 - 5.5f + col, jj * yOffset - 7*yOffset / 2f),
+                        //        Quaternion.identity);
+                        //}
+                        hex_go = (GameObject)Instantiate(
+                            block,
+                            new Vector2(
+                                ii *xOffset - 5.5f + col + (jj % 2) * xOffset / 2f,
+                                jj * yOffset + 3f),
+                            Quaternion.identity);
+                        // mod end nnvu
                         hex_go.name = "Base_" + ii + "_" + jj;
                         hex_go.transform.SetParent(this.transform);
                     }
